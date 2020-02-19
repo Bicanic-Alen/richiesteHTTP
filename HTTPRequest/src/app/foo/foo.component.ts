73937fbe45c5f29c76d27022838bf1fd990c6bf7
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-02-19 13:57:00 +0000

    punto2

--- HTTPRequest/src/app/foo/foo.component.ts
+++ HTTPRequest/src/app/foo/foo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Foo } from './foo.model';
 
 @Component({
   selector: 'app-foo',
@@ -10,8 +11,9 @@ import { HttpClient } from '@angular/common/http';
 export class FooComponent implements OnInit {
    data: Object;
    loading: boolean;
-   o :Observable<Object>;
-
+    o :Observable<Object>;
+    fooData : Foo[];
+    oFoo : Observable<Foo[]>;
   constructor(public http: HttpClient) { }
      makeRequest(): void {
      console.log("here");
@@ -20,11 +22,28 @@ export class FooComponent implements OnInit {
      this.o.subscribe(this.getData);
 
     }
+    makeTypedRequest() : void
+    {
+   //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+   this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+   this.oFoo.subscribe(data => {this.fooData = data;});
+    }
+
+    makeCompactRequest(): void {
+     this.loading = true;
+     this.http
+       .get('https://jsonplaceholder.typicode.com/posts/1')
+       .subscribe(newData => {
+       this.data = newData;
+       this.loading = false;
+       });
+    }
     getData = (d : Object) =>
     {
       this.data = new Object(d);
       this.loading = false;
     }
+
 
   ngOnInit() {
   }
